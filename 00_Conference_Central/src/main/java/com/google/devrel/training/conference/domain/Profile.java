0dc23cb74289f79f2cb74742218bab91757cbb11
garfieldua <andrey.davydenko@gmail.com>
2016-02-18 10:17:09 +0200

    Implement conference search and register functionality

--- 00_Conference_Central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ 00_Conference_Central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -1,9 +1,12 @@
 package com.google.devrel.training.conference.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
-
+import com.google.common.collect.ImmutableList;
 
 // TODO indicate that this class is an Entity
 @Entity
@@ -60,5 +63,29 @@ public class Profile {
      * Just making the default constructor private.
      */
     private Profile() {}
+    
+ // List of conferences the user has registered to attend
+    private List<String> conferenceKeysToAttend = new ArrayList<>(0);
+
+    public List<String> getConferenceKeysToAttend() {
+            return ImmutableList.copyOf(conferenceKeysToAttend);
+        }
+        
+        public void addToConferenceKeysToAttend(String conferenceKey) {
+            conferenceKeysToAttend.add(conferenceKey);
+        }
+        
+        /**
+         * Remove the conferenceId from conferenceIdsToAttend.
+         *
+         * @param conferenceKey a websafe String representation of the Conference Key.
+         */
+        public void unregisterFromConference(String conferenceKey) {
+            if (conferenceKeysToAttend.contains(conferenceKey)) {
+                conferenceKeysToAttend.remove(conferenceKey);
+            } else {
+                throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
+            }
+        }
 
 }
